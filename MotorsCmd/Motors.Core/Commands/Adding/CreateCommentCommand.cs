cec92dfd546fd07f833970e32319ddbb3b3519f0
Nikola Kolchakov <nikola.kolchakov@gmail.com>
2017-10-17 14:39:19 +0300

    create comment skele

--- MotorsCmd/Motors.Core/Commands/Adding/CreateCommentCommand.cs
+++ MotorsCmd/Motors.Core/Commands/Adding/CreateCommentCommand.cs
@@ -1,5 +1,6 @@
 ﻿using Bytes2you.Validation;
 using Motors.Core.Commands.Contracts;
+using Motors.Core.Providers.ConsoleInputProviders.Contracts;
 using Motors.Core.Providers.Contracts;
 using Motors.Data;
 using System;
@@ -10,19 +11,23 @@ namespace Motors.Core.Commands.Adding
     public class CreateCommentCommand : ICommand
     {
         private readonly IMotorSystemContext context;
+        private readonly ICommentInputProvider commentInputProvider;
         //private readonly SOMEMODELFACTORY factory;
 
-        public CreateCommentCommand(IMotorSystemContext context/*, SOMEMODELFACTORY factory*/)
+        public CreateCommentCommand(IWriter writer, IMotorSystemContext context/*, SOMEMODELFACTORY factory*/, ICommentInputProvider commentInputProvider)
         {
             Guard.WhenArgument(context, "context").IsNull().Throw();
+            Guard.WhenArgument(commentInputProvider, "commentInputProvider").IsNull().Throw();
             //Guard.WhenArgument(factory, "factory").IsNull().Throw();
-            
+
             this.context = context;
+            this.commentInputProvider = commentInputProvider;
             //this.factory = factory;
         }
         public string Execute()
         {
-            //var comment = this.factory.CreateComment(content);
+            var input = this.commentInputProvider.CreateCommentInput();
+            //var comment = this.factory.CreateComment(input);
             //this.context.Commets.Add(comment);
 
             try
@@ -31,7 +36,7 @@ namespace Motors.Core.Commands.Adding
             }
             catch
             {
-                Console.WriteLine("Failed to save comment in the database");
+                return "Failed to save comment in the database";
             }
 
             return "Comment with ID" +/*{this.context.Commets.Count-1}*/ "was created.";
