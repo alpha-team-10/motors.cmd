d4300d2864b113a203f0f6b366119a29b00bf87f
Dimitar Gochev <dimitarggochev@gmail.com>
2017-10-17 14:22:54 +0300

    added IModelFactory

---
+++ MotorsCmd/Motors.Core/Factories/Contracts/IModelFactory.cs
@@ -0,0 +1,20 @@
+﻿using Motors.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motors.Core.Factories.Contracts
+{
+    public interface IModelFactory
+    {
+        User CreateUser(string username, string password, string mail, string salt);
+
+        Offer CreateOffer(User user, Motorcycle motorcycle, decimal price, DateTime startingDate, DateTime endingDate);
+
+        Motorcycle CreateMotorcycle(Model model, int power, DateTime productionDate, decimal kilometers);
+
+        Comment CreateComment(string content);
+    }
+}
