740b19c8b596c75fc159fb27d4efca4a72a01cbf
Dimitar Gochev <dimitarggochev@gmail.com>
2017-10-18 17:12:50 +0300

    add createUserCommand + ModelFactory class

---
+++ MotorsCmd/Motors.Core/Commands/Adding/CreateUserCommand.cs
@@ -0,0 +1,40 @@
+﻿using Bytes2you.Validation;
+using Motors.Core.Commands.Contracts;
+using Motors.Core.Factories.Contracts;
+using Motors.Core.Providers.ConsoleInputProviders;
+using Motors.Core.Providers.ConsoleInputProviders.Contracts;
+using Motors.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motors.Core.Commands.Adding
+{
+    public class CreateUserCommand : ICommand
+    {
+        private readonly IMotorSystemContext context;
+        private readonly IUserInputProvider user;
+        private readonly IModelFactory userModel;
+
+        public CreateUserCommand(IMotorSystemContext context, IUserInputProvider user, IModelFactory userModel)
+        {
+            Guard.WhenArgument(context, "context").IsNull().Throw();
+            Guard.WhenArgument(user, "user").IsNull().Throw();
+            Guard.WhenArgument(userModel, "userModel").IsNull().Throw();
+
+            this.context = context;
+            this.user = user;
+            this.userModel = userModel;
+        }
+
+        public string Execute()
+        {
+            List<string> input = user.RegisterUserInput().ToList();
+            
+            
+            return "User with username was created!";
+        }
+    }
+}
---
+++ MotorsCmd/Motors.Core/Factories/ModelFactory.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Motors.Models;
+using Motors.Core.Factories.Contracts;
+
+namespace Motors.Core.Factories
+{
+    public class ModelFactory : IModelFactory
+    {
+        public Comment CreateComment(string content)
+        {
+            return new Comment()
+            {
+                Content = content
+            };
+        }
+
+
+        public Motorcycle CreateMotorcycle(Model model, int power, DateTime productionDate, decimal kilometers)
+        {
+            return new Motorcycle()
+            {
+                Model = model,
+                Power = power,
+                ProductionDate = productionDate,
+                Kilometers = kilometers
+            };
+        }
+
+        public Offer CreateOffer(User user, Motorcycle motorcycle, decimal price, DateTime startingDate, DateTime endingDate)
+        {
+            return new Offer
+            {
+                User = user,
+                Motorcycle = motorcycle,
+                Price = price,
+                StartDate = startingDate,
+                ExpireDate = endingDate
+            };
+        }
+
+        public User CreateUser(string username, string password, string mail, string salt)
+        {
+            return new User()
+            {
+                Username = username,
+                Password = password,
+                Mail = mail,
+                Salt = salt
+            };
+        }
+    }
+}
