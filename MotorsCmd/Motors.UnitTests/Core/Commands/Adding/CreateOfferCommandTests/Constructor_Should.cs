89aa03575888640d76467b583defc8600968a32c
Ditmar Vladislavov <didox222@gmail>
2017-10-18 15:30:38 +0300

    Added unit tests for adding, editing and deleting classes, but not completed

---
+++ MotorsCmd/Motors.UnitTests/Core/Commands/Adding/CreateOfferCommandTests/Constructor_Should.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Motors.Core.Commands.Adding;
+
+namespace Motors.UnitTests.Core.Commands.Adding.CreateOfferCommandTests
+{
+    [TestClass]
+    public class Constructor_Should
+    {
+        [TestMethod]
+        public void ReturnInstance_WhenCalled()
+        {
+            Assert.IsNotNull(new CreateCommentCommand());
+        }
+    }
+}
---
+++ MotorsCmd/Motors.UnitTests/Core/Commands/Deleting/DeleteMotorCommandTests/Constructor_Should.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Motors.Core.Commands.Deleting;
+
+namespace Motors.UnitTests.Core.Commands.Deleting.DeleteMotorCommandTests
+{
+    [TestClass]
+    public class Constructor_Should
+    {
+        [TestMethod]
+        public void ReturnInstance_WhenCalled()
+        {
+            Assert.IsNotNull(new DeleteMotorCommand());
+        }
+    }
+}
